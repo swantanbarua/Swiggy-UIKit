bd0afea0a4a3b75dfea16791e9c6a773d9051a8a
Swantan Barua <work.swantan@gmail.com>
2024-11-04 13:23:04 +0530

    Initial Commit

---
+++ Swiggy-UIKitTests/Swiggy_UIKitTests.swift
@@ -0,0 +1,17 @@
+//
+//  Swiggy_UIKitTests.swift
+//  Swiggy-UIKitTests
+//
+//  Created by Swantan Barua on 04/11/24.
+//
+
+import Testing
+@testable import Swiggy_UIKit
+
+struct Swiggy_UIKitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Swiggy-UIKitUITests/Swiggy_UIKitUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Swiggy_UIKitUITestsLaunchTests.swift
+//  Swiggy-UIKitUITests
+//
+//  Created by Swantan Barua on 04/11/24.
+//
+
+import XCTest
+
+final class Swiggy_UIKitUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
