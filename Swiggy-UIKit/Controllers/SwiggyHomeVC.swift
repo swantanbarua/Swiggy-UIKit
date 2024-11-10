9e237641f4a4e434bbf603931dfdd57b007f32b2
Swantan Barua <work.swantan@gmail.com>
2024-11-10 23:13:21 +0530

    Indicator view outlets added

--- Swiggy-UIKit/Controllers/SwiggyHomeVC.swift
+++ Swiggy-UIKit/Controllers/SwiggyHomeVC.swift
@@ -10,6 +10,13 @@ import UIKit
 class SwiggyHomeVC: UIViewController {
 
     // MARK: - IBOUTLETS
+    @IBOutlet weak var swiggyIndicatorView: UIView!
+    @IBOutlet weak var foodIndicatorView: UIView!
+    @IBOutlet weak var instamartIndicatorView: UIView!
+    @IBOutlet weak var dineoutIndicatorView: UIView!
+    @IBOutlet weak var cardIndicatorView: UIView!
+    @IBOutlet weak var wineStoresIndicatorView: UIView!
+    @IBOutlet weak var reorderIndicatorView: UIView!
     
     // MARK: - LIFECYCLE METHODS
     override func viewDidLoad() {
