fb4dd727388369c485dde3cda01c95fa5cbc3c80
Swantan Barua <work.swantan@gmail.com>
2024-11-10 13:30:50 +0530

    padding fix

---
+++ Swiggy-UIKit/Controllers/CardVC.swift
@@ -0,0 +1,18 @@
+//
+//  CardVC.swift
+//  Swiggy-UIKit
+//
+//  Created by Swantan Barua on 08/11/24.
+//
+
+import UIKit
+
+class CardVC: UIViewController {
+
+    // MARK: - IBOUTLETS
+    
+    // MARK: - LIFECYCLE METHODS
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
