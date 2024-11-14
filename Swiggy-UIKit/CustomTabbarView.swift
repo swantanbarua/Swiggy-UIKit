db293f28283168cce2840e58c5fae3a3c9a591fc
Swantan Barua <work.swantan@gmail.com>
2024-11-14 22:04:13 +0530

    Required init and override init written for Custom Tabbar View

--- Swiggy-UIKit/CustomTabbarView.swift
+++ Swiggy-UIKit/CustomTabbarView.swift
@@ -9,4 +9,11 @@ import UIKit
 
 final class CustomTabbarView: UIView {
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
 }
