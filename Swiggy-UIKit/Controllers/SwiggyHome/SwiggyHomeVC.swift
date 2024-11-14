fd26b3ffd2eec6f598a2921d4ad3f4e1501535db
Swantan Barua <work.swantan@gmail.com>
2024-11-15 01:25:41 +0530

    Reset all views

--- Swiggy-UIKit/Controllers/SwiggyHome/SwiggyHomeVC.swift
+++ Swiggy-UIKit/Controllers/SwiggyHome/SwiggyHomeVC.swift
@@ -8,26 +8,9 @@
 import UIKit
 
 class SwiggyHomeVC: UIViewController {
-
-    // MARK: - IBOUTLETS
-    @IBOutlet weak var swiggyTabbarItemView: CustomTabbarView!
-    @IBOutlet weak var foodTabbarItemView: CustomTabbarView!
-    @IBOutlet weak var instamartTabbarItemView: CustomTabbarView!
-    @IBOutlet weak var dineoutTabbarItemView: CustomTabbarView!
-    @IBOutlet weak var cardTabbarItemView: CustomTabbarView!
-    @IBOutlet weak var wineStoresTabbarItemView: CustomTabbarView!
-    @IBOutlet weak var reorderTabbarItemView: CustomTabbarView!
     
     // MARK: - LIFECYCLE METHODS
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        self.swiggyTabbarItemView.configureView(title: "Swiggy")
-        self.foodTabbarItemView.configureView(title: "Food")
-        self.instamartTabbarItemView.configureView(title: "Instamart")
-        self.dineoutTabbarItemView.configureView(title: "Dineout")
-        self.cardTabbarItemView.configureView(title: "Card")
-        self.wineStoresTabbarItemView.configureView(title: "Wine Stores")
-        self.reorderTabbarItemView.configureView(title: "Reorder")
     }
 }
